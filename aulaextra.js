4ecac47e943babbb1e6bfdf6e5d1d6efe064a1f5
Monkey322 <otta12@gmail.com>
2021-11-18 13:19:50 -0300

    Atividade aula extra

---
+++ aulaextra.js
@@ -0,0 +1,83 @@
+[{  
+
+    "id": 1,
+    "Nome": "Dota 2",
+    "Genero": "Moba",
+    "Ano": "2013",
+    "Deselvovedora": "Valve",
+
+    "id": 2,
+    "Nome": "Grand Theft Auto",
+    "Genero": "Ação-Aventura",
+    "Ano": ["1997, 1999, 2001, 2002, 2004, 2008, 2013"],
+    "Deselvovedora": "RockStar Games",
+
+    "id": 3,
+    "Nome": "Counter-Strike",
+    "Genero": "FPS",
+    "Ano": ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}] 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
